985844c457133eddcd7e8a7d1d302af9f555edec
GrumpyVortex <yosepsiregar714@gmail.com>
2025-04-10 16:06:13 +0700

    update SVG

---
+++ Web/laravel-with-firebase-auth/resources/views/items/rumahtangga.blade.php
@@ -0,0 +1,21 @@
+<svg viewBox="0 0 128 128" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet"
+                        width="100%" height="100%">
+                        <defs>
+                            <linearGradient id="gradient3" x1="0%" y1="0%" x2="100%"
+                                y2="100%">
+                                <stop offset="0%" stop-color="#15AFFF" />
+                                <stop offset="100%" stop-color="#80BF80" />
+                            </linearGradient>
+                        </defs>
+                        <g fill="url(#gradient3)">
+                            <path d="M61.3,99.1V63.8c0-4.5,3.6-8.1,8.1-8.1h24.4c4.5,0,8.1,3.6,8.1,8.1v35.3H61.3z" />
+                            <rect x="61.3" y="117.2" width="40.7" height="6.8" />
+                            <rect x="61.3" y="101.3" width="40.7" height="13.6" />
+                            <path
+                                d="M50,83.2c-5,0-9,4.1-9,9s4.1,9,9,9v18.1c0,2.5,2,4.5,4.5,4.5v3.4H59v-3.4v-3.4V92.3C59,87.3,54.9,83.2,50,83.2z" />
+                            <path
+                                d="M113.3,83.2c-5,0-9,4.1-9,9v28.3v3.4v3.4h4.5v-3.4c2.5,0,4.5-2,4.5-4.5v-18.1c5,0,9-4.1,9-9 S118.3,83.2,113.3,83.2z" />
+                            <polygon
+                                points="35.3,118.3 35.3,36.9 59,36.9 54.5,0.7 9.3,0.7 4.7,36.9 28.5,36.9 28.5,118.3 25.1,118.3 22.8,127.3 40.9,127.3 38.7,118.3" />
+                        </g>
+                    </svg>
